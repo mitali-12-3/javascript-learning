7376e07b7c727a43cc6fb0799de1c0a8bfc1b8bc
Mitali Pandey <mailtomitalipandey@gmail.com>
2024-11-09 13:16:31 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"
+//treat entire code as newer version of JS
+//works fine w/o writing as well
+
+// alert(3+3)
+//NOTE: don't since we are using Node, not browser
+
+console.log(3+3)
+console.log("Mitali")
+
+let name="Mitali"
+let age=20
+let isLoggedIn= true
+let state= null
+/*
+NOTE:
+1. number = 2 ^ 53
+2. bigint = for large numbers
+3. string = "assembly of characters"
+4. boolean = true or false
+5. null = stand alone value (type = object) representation of empty value
+6. undefined = kinda place holder for a value which is not defined yet (type = undefined)
+7. symbol = uniquness //used in react
+8. object
+ */
+
+console.log(typeof "Mitali")
+console.log(typeof age)
+console.log(typeof null) 
+//NOTE: null is Object
+console.log(typeof undefined)
+//NOTE: undefined is undefined type
