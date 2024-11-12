848fe592d5f71f34b0c4be67ebe0cc6c7ddf35c0
Mitali Pandey <mailtomitalipandey@gmail.com>
2024-11-12 12:26:27 +0000

    destructure and api intro

---
+++ 02_basics/05_destructure.js
@@ -0,0 +1,28 @@
+const course={
+    coursename:"btech in cse",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+//course.courseInstructor
+const{courseInstructor: instructor}= course
+console.log(instructor)
+
+//React destructure stuff
+const navbar=({company})=>{
+
+}
+navbar(company="Mitu")
+
+//API Stuff: backend value, JSON(object)
+// {
+//     "name":"Mitali Pandey",
+//     "coursename" :"Btech",
+//     "price":"Mehenga"
+
+// }
+//ye array :
+[
+    {},
+    {},
+    {}
+]
